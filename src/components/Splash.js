eac3fdc926169e0f386de895303b8e39ade6a2f1
Ronit Nagarapu <ronitnag04@gmail.com>
2022-12-16 21:08:05 -0800

    Started Components

---
+++ src/components/Splash.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    background:blue;
+    width:100vw;
+    height:100vh;
+`
+
+const Splash = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Splash
