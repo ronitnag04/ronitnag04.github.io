761f8f5b18cd9f15dec46e967f80d160f23067b4
Ronit Nagarapu <ronitnag04@gmail.com>
2022-12-18 05:59:03 -0600

    Splash Placeholder

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
+import { Container } from 'react-bootstrap'
 
-const Container = styled.div`
-    background:${props => props.color || 'white'};
-    width:100%;
-    height:7000px;
-`
 
 const Splash = (props) => {
   return (
-    <Container
-      color={props.color}
-    />
+    <Container>
+      <h1>Hello!</h1>
+      <h3>Thanks for visiting my page.</h3>
+      <h3>It is still under construction, so please check back later</h3>
+      <h3>See you soon!</h3>
+    </Container>
   )
 }
 
