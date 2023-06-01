f5927c50b151be960178f5aa2ec751568064ea8a
ronitnag04 <ronitnag04@gmail.com>
2023-05-31 21:43:14 -0700

    Stable HashRouter multipage build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import NavbarC from './components/NavbarC';
 import Splash from './components/Splash';
@@ -7,23 +7,23 @@ import Experience from './pages/experience';
 import Projects from './pages/projects';
 import About from './pages/about';
 import Contact from './pages/contact';
-import NoPage from './pages/404';
 import FooterC from './components/FooterC';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavbarC />
-      <Routes>
-        <Route path="/" element={<Splash />}/>
-          <Route path="experience" element={<Experience />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-      </Routes>
-      <FooterC />
-    </BrowserRouter>
+    <div className="App">
+      <HashRouter>
+        <NavbarC />
+        <Routes>
+          <Route path="/" element={<Splash />}/>
+            <Route path="experience" element={<Experience />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+        </Routes>
+        <FooterC />
+      </HashRouter>
+    </div>
   );
 }
 
--- src/components/NavbarC.js
+++ src/components/NavbarC.js
@@ -13,11 +13,11 @@ const NavbarC = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
             <Nav>
-                <Nav.Link href="./">Home</Nav.Link>
-                <Nav.Link href="/experience">Experience</Nav.Link>
-                <Nav.Link href="/projects">Projects</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/#/experience">Experience</Nav.Link>
+                <Nav.Link href="/#/projects">Projects</Nav.Link>
+                <Nav.Link href="/#/about">About</Nav.Link>
+                <Nav.Link href="/#/contact">Contact</Nav.Link>
             </Nav>
         </Navbar.Collapse>
       </Container>
